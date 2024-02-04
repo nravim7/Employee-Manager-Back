65962ab37d628587403d9b4b7f3de1dc9760e947
Ravi N <your.email@example.com>
2024-02-05 01:58:48 +0530

    CrossOrigin

---
+++ src/test/java/com/getarrays/employeemanager/repo/EmployeeRepoTest.java
@@ -0,0 +1,24 @@
+package com.getarrays.employeemanager.repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeRepoTest {
+    @Autowired
+    private EmployeeRepo underTest;
+
+    @Test
+    void deleteEmployeeById() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    void findEmployeeById() {
+    }
+}
